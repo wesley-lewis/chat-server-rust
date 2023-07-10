bb53d6c6a24b6ea40634b4344eaab80c6cb53552
wesley-lewis <lewis.wes.23@gmail.com>
2023-07-10 22:06:01 +0530

    Enabled multiple users connection

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
+use tokio::io::BufReader;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
     // echo server. Waits for a client to connect then sends a message back to the client.
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
     loop {
-        let mut buffer = [0u8; 1024];
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
 
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+            let mut reader = BufReader::new(reader);
+
+            let mut line = String::new();
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                writer.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
